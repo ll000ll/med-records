2b45746b6a251421dd3cef2fec84ce45a1dd2a8b
Mihail <mail.mihail@pm.me>
2022-07-09 17:12:40 +0300

    protect routes via session value

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -2,14 +2,15 @@ const express = require("express")
 
 const router = express.Router()
 const adminController = require("../controllers/admin-controller")
+const isAuth = require("../middleware/isAuth")
 
-router.get("/", adminController.getIndex)
-router.post("/", adminController.postIndex)
-router.get("/users/list", adminController.getAllUsers)
-router.get("/users/create", adminController.getCreateUser)
-router.post("/users/create", adminController.postCreateUser)
-router.get("/users/edit/:userId", adminController.getEditUser)
-router.post("/users/edit/:userId", adminController.postEditUser)
-router.post("/users/delete/:userId", adminController.deleteUser)
+router.get("/", isAuth, adminController.getIndex)
+router.post("/", isAuth, adminController.postIndex)
+router.get("/users/list", isAuth, adminController.getAllUsers)
+router.get("/users/create", isAuth, adminController.getCreateUser)
+router.post("/users/create", isAuth, adminController.postCreateUser)
+router.get("/users/edit/:userId", isAuth, adminController.getEditUser)
+router.post("/users/edit/:userId", isAuth, adminController.postEditUser)
+router.post("/users/delete/:userId", isAuth, adminController.deleteUser)
 
 module.exports = router
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,11 +2,12 @@ const express = require("express")
 
 const router = express.Router()
 const authController = require("../controllers/auth-controller")
+const isAuth = require("../middleware/isAuth")
 
 router.get("/signup", authController.getSignup)
 router.post("/signup", authController.postSignup)
 router.get("/login", authController.getLogin)
 router.post("/login", authController.postLogin)
-router.get("/logout", authController.logout)
+router.get("/logout", isAuth, authController.logout)
 
 module.exports = router
